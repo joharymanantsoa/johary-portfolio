729acfea0a5887ca29ced9a57a7759230b56eb73
joharymanantsoa <108999158+joharymanantsoa@users.noreply.github.com>
2025-07-03 08:04:52 +0300

    Update Technologie.jsx responsive

--- src/components/Technologie/Technologie.jsx
+++ src/components/Technologie/Technologie.jsx
@@ -17,7 +17,7 @@ function Technologie() {
                     What can I use...
                 </div>
                 <div className="flex flex-wrap justify-center">
-                    <div className="tech  md:h-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5 object-cover">
+                    <div className="tech  height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5 object-cover">
                         <img src={react} alt="react logo" />
                     </div>
                     <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
@@ -26,21 +26,21 @@ function Technologie() {
                     <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={javascript} alt="javascript logo" />
                     </div>
-                    <div className="tech height-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
+                    <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={nodejs} alt="nodejs logo" />
                     </div>
                     {/* </div> */}
                     {/* <div className="flex"> */}
-                    <div className="tech height-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
+                    <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={mysql} alt="mysql logo" />
                     </div>
-                    <div className="tech height-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
+                    <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={symfony} alt="symfony logo" />
                     </div>
-                    <div className="tech height-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
+                    <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={python} alt="python logo" />
                     </div>
-                    <div className="tech height-[256px] w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
+                    <div className="tech height-[128px] w-[128px] md:h-[256px] md:w-[256px] rounded-lg overflow-hidden flex-shrink-0 m-5">
                         <img src={wordpress} alt="wordpress logo" />
                     </div>
                 </div>
